23e215fc9ee1aac9383f5c90465f66d1e174c7a5
Dima Shtempelyuk <dimon.shtempelyuk@gmail.com>
2021-03-07 22:08:28 +0100

    FinalVers

--- Shoppinglist/Form1.Designer.cs
+++ Shoppinglist/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.But = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // But
+            // 
+            this.But.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.But.Location = new System.Drawing.Point(19, 35);
+            this.But.Name = "But";
+            this.But.Size = new System.Drawing.Size(25, 25);
+            this.But.TabIndex = 0;
+            this.But.Text = "+";
+            this.But.UseVisualStyleBackColor = true;
+            this.But.Click += new System.EventHandler(this.But_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nakup si:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hotovo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.But);
+            this.MaximumSize = new System.Drawing.Size(450, 450);
+            this.MinimumSize = new System.Drawing.Size(450, 450);
+            this.Name = "Form1";
+            this.Text = "Nákupní seznam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button But;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Shoppinglist/Form1.cs
+++ Shoppinglist/Form1.cs
@@ -10,11 +10,83 @@ using System.Windows.Forms;
 
 namespace Shoppinglist
 {
+    
     public partial class Form1 : Form
     {
+        public List<TextBox> TextBoxList = new List<TextBox>();
+        public List<CheckBox> CheckboxList = new List<CheckBox>();
+        public List<Button> ButtonList = new List<Button>();
+        int number = 0;
         public Form1()
         {
-            InitializeComponent();
+         InitializeComponent();
+            
+        }
+
+        private void But_Click(object sender, EventArgs e)
+        {
+           
+            TextBox tb = new TextBox();
+            string Tpolozky = "Text" + number;
+            tb.Name = Tpolozky;
+
+            tb.Left = 20;
+            tb.Top = But.Top + 0;
+            tb.Width = 300;
+            TextBoxList.Add(tb);
+            Controls.Add(tb);
+
+            CheckBox cb = new CheckBox();
+            string CBcpolozky = "CheckBox" + number;
+            cb.Name = CBcpolozky;
+            CheckboxList.Add(cb);
+            cb.Left = 340;
+            cb.Width = 20;
+            cb.Top = But.Top + 0;
+            Controls.Add(cb);
+
+            Button bl = new Button();
+           int Bcpolozky = number;
+            bl.Name = Convert.ToString(Bcpolozky);
+            ButtonList.Add(bl);
+            bl.Width = 50;
+            bl.Left = 375;
+            bl.Text = "Smazat";
+            bl.Top = But.Top + 0;
+            Controls.Add(bl);
+            bl.Click += new EventHandler (this.Bcpolozky_Click);
+            But.Top += 30;
+            number++;
+        }
+        void Bcpolozky_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            for (int bCount = 0; bCount < ButtonList.Count; bCount++)
+            {
+                if (btn == ButtonList[bCount])
+                {
+                    this.Controls.Remove(TextBoxList[bCount]);
+                    TextBoxList.RemoveAt(bCount);
+                    this.Controls.Remove(CheckboxList[bCount]);
+                    CheckboxList.RemoveAt(bCount);
+                    this.Controls.Remove(ButtonList[bCount]);
+                    ButtonList.RemoveAt(bCount);
+                    number--;
+                    try { 
+                    for (int i = bCount; i < ButtonList.Count; i++)
+                        {
+                        TextBoxList[i].Top -= 30;
+                        CheckboxList[i].Top -= 30;
+                        ButtonList[i].Top -= 30;
+                        }
+                        But.Top -= 30;
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
         }
     }
 }
